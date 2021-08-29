147e00e8f45bca762f33ad9958bf9586e35e7283
Mitchell Mudd <mitchellmudd@outlook.com>
2021-08-29 10:25:08 -0400

    Learned how to add images and fonts. Struggled to get fonts to work but added a p tag and changed to a smaller font file and it works now :)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 // import your function
-import myName from './myName';
+import _ from 'lodash';
 import './style.css'
+import picture from './download.jpeg'
 
 function component() {
   const element = document.createElement('div');
+  element.classList.add('doggy')
+  const text = document.createElement('p');
+  // Lodash, now imported by this script
+  text.textContent = _.join(['Hello', 'webpack'], ' ');
+  text.classList.add('hello');
+  // Add the image to our existing div.
+  const myPic = new Image();
+  myPic.src = picture;
 
-  // use your function!
-  element.innerHTML = myName('Cody');
-  element.classList.add('hello')
+  element.appendChild(text);
+  element.appendChild(myPic);
+
   return element;
 }
 
