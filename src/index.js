dc4fc57dd95a1c2366795496d7738fd1401cc9cc
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-03 19:38:15 -0400

    Finished the output management section of the webpack tutorials

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 // import your function
 import _ from 'lodash';
+import printMe from './print.js'
 
 function component() {
   const element = document.createElement('div');
   const text = document.createElement('p');
   text.textContent = _.join(['Hello', 'webpack'], ' ');
-
+  const button = document.createElement('button')
+  button.innerHTML = 'click me and check the console!'
+  button.onclick = printMe;
   element.appendChild(text);
+  element.appendChild(button);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
     },
+    plugins: [
+        new HtmlWebPackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'development'
 };
+
